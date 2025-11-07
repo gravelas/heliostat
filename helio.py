48a5c756345b7799340f04bbeead3a10cac69154
Jason Sima <jasonsima2006@gmail.com>
2025-11-07 11:10:52 -0500

    fix stuff

--- helio.py
+++ helio.py
@@ -12,33 +12,32 @@ servo = PWM(Pin(18))
 servo.freq(50)  # 50Hz for servo control
 
 # Servo duty cycle constants (adjust for your servo)
-MIN_DUTY = 1802   # 0 degrees
-MAX_DUTY = 7864   # 180 degrees
+MIN_DUTY = 1802  # 0 degrees
+MAX_DUTY = 7864  # 180 degrees
 
-angle=90
+angle = 90
+
 
 def map_value(value, from_min, from_max, to_min, to_max):
-  """Map a value from one range to another"""
-  return int((value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min)
+    """Map a value from one range to another"""
+    return int((value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min)
 
-while (True):
-  duty = map_value(angle, 0, 180, MIN_DUTY, MAX_DUTY)
-  servo.duty_u16(duty)
-  light_level_left = pr_l.read_u16()
-  light_level_right = pr_r.read_u16()
-  light_level_center = pr_c.read_u16()
-  
-  if (light_level_right == light_level_left):
-    pass
-  elif (light_level_right > light_level_left):
-    if (angle > 0): 
-      angle -= 1
-  elif (light_level_left > light_level_right):
-    if (angle < 180):
-      angle += 1
-  
-  print("LIGHT VALUE: " + str(light_level_center))
-  print("RIGHT: " + str(light_level_right))
-  print("LEFT: " + str(light_level_left))
-  time.sleep(.1)
 
+while True:
+    duty = map_value(angle, 0, 180, MIN_DUTY, MAX_DUTY)
+    servo.duty_u16(duty)
+    light_level_left = pr_l.read_u16() + 1700
+    light_level_right = pr_r.read_u16()
+    light_level_center = pr_c.read_u16()
+
+    if light_level_right == light_level_left:
+        pass
+    elif light_level_right > light_level_left:
+        if angle > 0:
+            angle -= 1
+    elif light_level_left > light_level_right:
+        if angle < 180:
+            angle += 1
+
+    print("LIGHT VALUE: " + str(light_level_center))
+    time.sleep(.025)
